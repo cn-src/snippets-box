695ccedfa9ee0f019c5818c01c4eb8a39416f629
cn-src <public@javaer.cn>
2019-12-09 16:17:11 +0800

    Update CurrentUserProvider.java

--- src/main/java/cn/javaer/snippetsbox/jooq/CurrentUserProvider.java
+++ src/main/java/cn/javaer/snippetsbox/jooq/CurrentUserProvider.java
@@ -6,5 +6,10 @@ package cn.javaer.snippetsbox.jooq;
 @FunctionalInterface
 public interface CurrentUserProvider {
 
+    /**
+     * 获取当前用户.
+     *
+     * @return 当前用户
+     */
     Object currentUser();
 }
