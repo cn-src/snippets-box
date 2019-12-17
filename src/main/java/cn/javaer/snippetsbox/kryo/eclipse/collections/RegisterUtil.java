73f698f63f9ec463929d29af1e8d6d2540851f36
cn-src <public@javaer.cn>
2019-12-17 17:09:45 +0800

    kryo support

---
+++ src/main/java/cn/javaer/snippetsbox/kryo/eclipse/collections/RegisterUtil.java
@@ -0,0 +1,26 @@
+package cn.javaer.snippetsbox.kryo.eclipse.collections;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfoList;
+import io.github.classgraph.ScanResult;
+
+/**
+ * @author cn-src
+ */
+class RegisterUtil {
+    private RegisterUtil() {}
+
+    static void register(final Kryo kryo, final Serializer<?> serializer, final Class<?> interfaceClass, final String... scanPackage) {
+        try (final ScanResult scanResult = new ClassGraph().enableClassInfo().ignoreClassVisibility().whitelistPackages(scanPackage)
+                .scan()) {
+            final ClassInfoList classInfos = scanResult.getClassesImplementing(interfaceClass.getName())
+                    .filter(it -> !it.isAbstract() && !it.isInterface());
+
+            for (final Class<?> clazz : classInfos.loadClasses()) {
+                kryo.register(clazz, serializer);
+            }
+        }
+    }
+}
