477251f38aa3e5eef14d73c48d86aae5f639b6bd
cn-src <public@javaer.cn>
2019-12-25 17:07:50 +0800

    clean code

--- src/main/java/cn/javaer/snippetsbox/springframework/boot/jooq/JooqDSLContextAutoConfiguration.java
+++ src/main/java/cn/javaer/snippetsbox/springframework/boot/jooq/JooqDSLContextAutoConfiguration.java
@@ -1,30 +1,16 @@
 package cn.javaer.snippetsbox.springframework.boot.jooq;
 
-import org.jooq.ConnectionProvider;
 import org.jooq.DSLContext;
-import org.jooq.ExecuteListenerProvider;
-import org.jooq.ExecutorProvider;
-import org.jooq.RecordListenerProvider;
-import org.jooq.RecordMapperProvider;
-import org.jooq.RecordUnmapperProvider;
-import org.jooq.TransactionListenerProvider;
-import org.jooq.TransactionProvider;
-import org.jooq.VisitListenerProvider;
 import org.jooq.conf.Settings;
 import org.jooq.impl.DefaultConfiguration;
 import org.jooq.impl.DefaultDSLContext;
-import org.jooq.impl.DefaultExecuteListenerProvider;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.jooq.JooqExceptionTranslator;
 import org.springframework.boot.autoconfigure.jooq.JooqProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-
-import javax.sql.DataSource;
 
 /**
  * 将 jOOQ 仅用于 SQL 生成时的自动配置（即：没有数据源）.
@@ -38,15 +24,15 @@ import javax.sql.DataSource;
 public class JooqDSLContextAutoConfiguration {
 
     @Bean
-    public DefaultDSLContext dslContext(org.jooq.Configuration configuration) {
+    public DefaultDSLContext dslContext(final org.jooq.Configuration configuration) {
         return new DefaultDSLContext(configuration);
     }
 
     @Bean
     @ConditionalOnMissingBean(org.jooq.Configuration.class)
-    public DefaultConfiguration jooqConfiguration(JooqProperties properties,
-                                                  ObjectProvider<Settings> settings) {
-        DefaultConfiguration configuration = new DefaultConfiguration();
+    public DefaultConfiguration jooqConfiguration(final JooqProperties properties,
+                                                  final ObjectProvider<Settings> settings) {
+        final DefaultConfiguration configuration = new DefaultConfiguration();
         configuration.setSQLDialect(properties.getSqlDialect());
         settings.ifAvailable(configuration::set);
         return configuration;
