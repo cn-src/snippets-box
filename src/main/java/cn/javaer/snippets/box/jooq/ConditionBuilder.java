6472021389adbb1db9cceff961604835788190bb
cn-src <public@javaer.cn>
2020-04-07 18:49:22 +0800

    update

--- src/main/java/cn/javaer/snippets/box/jooq/ConditionBuilder.java
+++ src/main/java/cn/javaer/snippets/box/jooq/ConditionBuilder.java
@@ -1,17 +1,16 @@
 package cn.javaer.snippets.box.jooq;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jooq.Condition;
 import org.springframework.lang.Nullable;
 import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -20,7 +19,15 @@ import java.util.function.Supplier;
  * @author cn-src
  */
 public class ConditionBuilder {
+
+    @FunctionalInterface
+    public interface Function3<T1, T2, T3>
+            extends Serializable {
+        Condition apply(T1 argument1, T2 argument2, T3 argument3);
+    }
+
     private final List<Condition> conditions = new ArrayList<>();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public ConditionBuilder append(final Supplier<Condition> supplier) {
         this.conditions.add(supplier.get());
@@ -36,49 +43,29 @@ public class ConditionBuilder {
         return this;
     }
 
-    @SafeVarargs
-    public final <T> ConditionBuilder append(final Function<T[], Condition> fun, final T... values) {
-        if (ObjectUtils.isEmpty(values)) {
+    public <T1, T2> ConditionBuilder append(final BiFunction<T1, T2, Condition> fun, final T1 t1, final T2 t2) {
+        if (null == t1 || null == t2) {
             return this;
         }
-
-        //noinspection unchecked
-        final T[] objects = (T[]) Arrays.stream(values)
-                .filter(Objects::nonNull)
-                .toArray();
-
-        if (objects.length == 0) {
-            return this;
-        }
-
-        this.conditions.add(fun.apply(objects));
+        this.conditions.add(fun.apply(t1, t2));
         return this;
     }
 
-    public ConditionBuilder append(final Function<String[], Condition> fun, final String... values) {
-        if (ObjectUtils.isEmpty(values)) {
-            return this;
-        }
-
-        final String[] toArray = Arrays.stream(values)
-                .filter(StringUtils::hasLength)
-                .toArray(String[]::new);
-
-        if (toArray.length == 0) {
-            return this;
-        }
-
-        this.conditions.add(fun.apply(toArray));
-        return this;
-    }
-
-    public ConditionBuilder append(final BiFunction<LocalDateTime, LocalDateTime, Condition> fun, final LocalDate start, final LocalDate end) {
+    public ConditionBuilder dateTime(final BiFunction<LocalDateTime, LocalDateTime, Condition> fun, final LocalDate start, final LocalDate end) {
         if (null == start || null == end) {
             return this;
         }
         final LocalDateTime startTime = start.atStartOfDay();
         final LocalDateTime endTime = end.atTime(LocalTime.MAX);
         this.conditions.add(fun.apply(startTime, endTime));
+        return this;
+    }
+
+    public <T1, T2, T3> ConditionBuilder append(final Function3<T1, T2, T3> fun, final T1 t1, final T2 t2, final T3 t3) {
+        if (t1 == null || t2 == null || t3 == null) {
+            return this;
+        }
+        this.conditions.add(fun.apply(t1, t2, t3));
         return this;
     }
 
