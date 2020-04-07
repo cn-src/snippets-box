3050415f37c885c604018ed9bb4ede23f7fcb965
cn-src <public@javaer.cn>
2020-04-07 12:32:12 +0800

    update

--- src/main/java/cn/javaer/snippets/box/jooq/ConditionBuilder.java
+++ src/main/java/cn/javaer/snippets/box/jooq/ConditionBuilder.java
@@ -10,20 +10,15 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.LongFunction;
 import java.util.function.Supplier;
 
 /**
  * @author cn-src
  */
 public class ConditionBuilder {
-    public interface ConditionFunction<T> {
-        Condition apply(T t);
-    }
-
-    public interface ConditionArrayFunction<T> {
-        Condition apply(T[] t);
-    }
-
     private final List<Condition> conditions = new ArrayList<>();
 
     public ConditionBuilder append(final Supplier<Condition> supplier) {
@@ -31,7 +26,7 @@ public class ConditionBuilder {
         return this;
     }
 
-    public <T> ConditionBuilder append(final ConditionFunction<T> fun, final T value) {
+    public <T> ConditionBuilder append(final Function<T, Condition> fun, final T value) {
         if (ObjectUtils.isEmpty(value)) {
             return this;
         }
@@ -40,7 +35,36 @@ public class ConditionBuilder {
         return this;
     }
 
-    public <T> ConditionBuilder append(final ConditionArrayFunction<T> fun, final T... array) {
+    public ConditionBuilder append(final IntFunction<Condition> fun, final int value) {
+        this.conditions.add(fun.apply(value));
+        return this;
+    }
+
+    public ConditionBuilder append(final LongFunction<Condition> fun, final long value) {
+        this.conditions.add(fun.apply(value));
+        return this;
+    }
+
+    @SafeVarargs
+    public final <T> ConditionBuilder append(final Function<T[], Condition> fun, final T... array) {
+        if (null == array || array.length == 0) {
+            return this;
+        }
+
+        this.conditions.add(fun.apply(array));
+        return this;
+    }
+
+    public ConditionBuilder append(final Function<int[], Condition> fun, final int... array) {
+        if (null == array || array.length == 0) {
+            return this;
+        }
+
+        this.conditions.add(fun.apply(array));
+        return this;
+    }
+
+    public ConditionBuilder append(final Function<long[], Condition> fun, final long... array) {
         if (null == array || array.length == 0) {
             return this;
         }
