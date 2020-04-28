8ab1a55a73817f798936c1188e1b73888d359901
cn-src <public@javaer.cn>
2020-04-28 23:55:15 +0800

    update

--- src/main/java/cn/javaer/snippets/box/spring/data/jooq/jdbc/JsonbConverters.java
+++ src/main/java/cn/javaer/snippets/box/spring/data/jooq/jdbc/JsonbConverters.java
@@ -1,5 +1,8 @@
 package cn.javaer.snippets.box.spring.data.jooq.jdbc;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jooq.JSONB;
 import org.postgresql.util.PGobject;
 import org.springframework.core.convert.converter.Converter;
@@ -15,10 +18,13 @@ import java.util.List;
  */
 public abstract class JsonbConverters {
     private static final List<Converter<?, ?>> CONVERTERS = new ArrayList<>();
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     static {
         JsonbConverters.CONVERTERS.add(ToJsonbConverter.INSTANCE);
         JsonbConverters.CONVERTERS.add(JsonbToConverter.INSTANCE);
+        JsonbConverters.CONVERTERS.add(ToJsonNodeConverter.INSTANCE);
+        JsonbConverters.CONVERTERS.add(JsonNodeToConverter.INSTANCE);
     }
 
     public static List<Converter<?, ?>> getConvertersToRegister() {
@@ -26,7 +32,7 @@ public abstract class JsonbConverters {
     }
 
     @ReadingConverter
-    private enum ToJsonbConverter implements Converter<PGobject, JSONB> {
+    public enum ToJsonbConverter implements Converter<PGobject, JSONB> {
 
         /**
          * 单实例.
@@ -40,7 +46,7 @@ public abstract class JsonbConverters {
     }
 
     @WritingConverter
-    private enum JsonbToConverter implements Converter<JSONB, String> {
+    public enum JsonbToConverter implements Converter<JSONB, String> {
 
         /**
          * 单实例.
@@ -50,6 +56,44 @@ public abstract class JsonbConverters {
         @Override
         public String convert(final JSONB source) {
             return source.data();
+        }
+    }
+
+    @ReadingConverter
+    public enum ToJsonNodeConverter implements Converter<PGobject, JsonNode> {
+
+        /**
+         * 单实例.
+         */
+        INSTANCE;
+
+        @Override
+        public JsonNode convert(final PGobject source) {
+            try {
+                return OBJECT_MAPPER.readTree(source.getValue());
+            }
+            catch (final JsonProcessingException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+    }
+
+    @WritingConverter
+    public enum JsonNodeToConverter implements Converter<JsonNode, String> {
+
+        /**
+         * 单实例.
+         */
+        INSTANCE;
+
+        @Override
+        public String convert(final JsonNode source) {
+            try {
+                return OBJECT_MAPPER.writeValueAsString(source);
+            }
+            catch (final JsonProcessingException e) {
+                throw new IllegalStateException(e);
+            }
         }
     }
 }
