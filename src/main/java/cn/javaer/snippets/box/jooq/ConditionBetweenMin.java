77f93eb0604e65d6261e729d58c8e7a2503ecd64
cn-src <public@javaer.cn>
2020-06-09 17:55:34 +0800

    update

---
+++ src/main/java/cn/javaer/snippets/box/jooq/ConditionBetweenMin.java
@@ -0,0 +1,21 @@
+package cn.javaer.snippets.box.jooq;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author cn-src
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ConditionBetweenMin {
+
+    /**
+     * Between 的表列名，驼峰式自动转换成下划线。
+     *
+     * @return Between 的表列名
+     */
+    String value();
+}
--- src/main/java/cn/javaer/snippets/box/jooq/ConditionCreator.java
+++ src/main/java/cn/javaer/snippets/box/jooq/ConditionCreator.java
@@ -1,5 +1,6 @@
 package cn.javaer.snippets.box.jooq;
 
+import lombok.Data;
 import org.jooq.Condition;
 import org.jooq.Field;
 import org.jooq.JSONB;
@@ -11,7 +12,9 @@ import org.springframework.util.ReflectionUtils;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Condition 条件创建器，根据 POJO 来动态创建条件.
@@ -36,6 +39,7 @@ public class ConditionCreator {
         final List<Condition> conditions = new ArrayList<>();
         final Class<?> clazz = query.getClass();
         final PropertyDescriptor[] descriptors = BeanUtils.getPropertyDescriptors(clazz);
+        final Map<String, BetweenValue> betweenValueMap = new HashMap<>();
         try {
             for (final PropertyDescriptor dr : descriptors) {
                 final String name = dr.getName();
@@ -64,6 +68,24 @@ public class ConditionCreator {
                     //noinspection unchecked
                     conditions.add(Sql.arrayContained(jooqField, (String[]) value));
                 }
+                else if (field.getAnnotation(ConditionBetweenMin.class) != null) {
+                    final ConditionBetweenMin betweenMin = field.getAnnotation(ConditionBetweenMin.class);
+                    BetweenValue betweenValue = betweenValueMap.get(betweenMin.value());
+                    if (null == betweenValue) {
+                        betweenValue = new BetweenValue();
+                        betweenValueMap.put(betweenMin.value(), betweenValue);
+                    }
+                    betweenValue.setMin(value);
+                }
+                else if (field.getAnnotation(ConditionBetweenMax.class) != null) {
+                    final ConditionBetweenMax betweenMax = field.getAnnotation(ConditionBetweenMax.class);
+                    BetweenValue betweenValue = betweenValueMap.get(betweenMax.value());
+                    if (null == betweenValue) {
+                        betweenValue = new BetweenValue();
+                        betweenValueMap.put(betweenMax.value(), betweenValue);
+                    }
+                    betweenValue.setMax(value);
+                }
                 else {
                     if (!ignoreUnannotated) {
                         //noinspection unchecked
@@ -83,6 +105,30 @@ public class ConditionCreator {
         for (int i = 1, size = conditions.size(); i < size; i++) {
             condition = condition.and(conditions.get(i));
         }
+        for (final Map.Entry<String, BetweenValue> entry : betweenValueMap.entrySet()) {
+            final BetweenValue value = entry.getValue();
+            condition = condition.and(DSL.field(underline(entry.getKey())).between(value.getMin(), value.getMax()));
+        }
         return condition;
+    }
+
+    private static String underline(final String str) {
+        final char[] chars = str.toCharArray();
+        final StringBuilder sb = new StringBuilder(chars.length);
+        for (final char c : chars) {
+            if (Character.isUpperCase(c)) {
+                sb.append('_').append(Character.toLowerCase(c));
+            }
+            else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    @Data
+    static class BetweenValue {
+        Object min;
+        Object max;
     }
 }
