257405a548b1b3cfce700385048c3f2fc8336faa
cn-src <public@javaer.cn>
2020-06-30 22:12:47 +0800

    update docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    themeConfig: {
+        displayAllHeaders: true,
+        sidebar: [
+            '/',
+            '/jooq',
+            '/jooq-data-jdbc',
+            '/date-format',
+            '/spring',
+            '/springdoc',
+            '/kryo'
+        ]
+    }
+}
---
+++ docs/kryo.md
@@ -0,0 +1,3 @@
+# Kryo 序列化
+
+支持 eclipse-collections 的集合类型的序列化/反序列化
